8a9897c7e34b6358efcdb55f275bb66ede2e0e9f
Envolti.Luiz <luiz.pereira@envolti.com.br>
2020-02-16 18:18:55 -0300

    Adicionando Api1

---
+++ Api/Api/Controllers/ApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ApiController : Controller
+    {
+        [HttpGet("[action]")]
+        public ActionResult<string> TaxaJuros()
+        {
+            return "0,01";
+        }
+    }
+}
